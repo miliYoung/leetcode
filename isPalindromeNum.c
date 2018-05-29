1d2e55786ebcaf7902cd86a5bb29c83b7e2a1337
Mili Young <miliyang1224@163.com>
2018-05-29 23:46:17 +0800

    isPalindromeNum.c

---
+++ isPalindromeNum.c
@@ -0,0 +1,46 @@
+/**
+ * 	LEETCODE 9. Palindrome Number
+ */
+
+/**
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ * 
+ * Example 1:
+ * Input: 121
+ * Output: true
+ * 
+ * Example 2:
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * 
+ * Example 3:
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
+
+bool isPalindrome(int x)
+{
+	if( x < 0 )
+	{
+		return false;
+	}
+            
+    long res = 0;
+    int temp = x;
+    while( x/10 != 0)
+    {
+        res = res*10 + x%10;
+        x = x/10;
+    }
+    res = res *10 + x;
+        
+   	if(res > INT_MAX)
+        return false;
+    
+    if(res == temp)
+        return true;
+    else
+        return false;
+}
