ead3826a0be3a3e1e3ba37ea40cbfd607835c658
Mili Young <miliyang1224@163.com>
2018-06-10 14:33:18 +0800

    add

---
+++ isPowerOfTwo.c
@@ -0,0 +1,36 @@
+/**
+ * 	LEETCODE 231. Power of Two
+ */
+
+
+/**
+ * Given an integer, write a function to determine if it is a power of two.
+ * 
+ * Example 1:
+ * Input: 1
+ * Output: true 
+ * Explanation: 20 = 1
+ * 
+ * Example 2:
+ * Input: 16
+ * Output: true
+ * Explanation: 24 = 16
+ * 
+ * Example 3:
+ * Input: 218
+ * Output: false
+ */
+
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0) return false;
+
+ 	int cnt = 0;
+	while (n > 0)
+ 	{
+		cnt = cnt + (n & 0x1);
+		n = n >> 1;
+	}
+
+	return cnt == 1;
+}
