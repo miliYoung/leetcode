288eda208a4d870c45f08b8bb86ab768b5886b2c
Mili Young <miliyang1224@163.com>
2018-06-10 11:19:28 +0800

    add maxDepth.c

---
+++ maxDepth.c
@@ -0,0 +1,44 @@
+ /**
+  *  LEETCODE 104. Maximum Depth of Binary Tree
+  */
+
+/**
+ * Given a binary tree, find its maximum depth.
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *    3
+ *   / \
+ *  9  20
+ *    /  \
+ *   15   7
+ * return its depth = 3.
+ */
+
+
+ int maxDepth(TreeNode* root) {
+    if(root == NULL) return 0;
+        
+    int left = 0;
+    int right = 0;
+    if(root->left != NULL)
+    {
+        left = maxDepth(root->left);
+    }
+        
+    if(root->right != NULL)
+    {
+        right = maxDepth(root->right);
+    }
+        
+    if(left >= right)
+    {
+        return left+1;
+    }
+    else
+    {
+        return right+1; 
+    }
+}
